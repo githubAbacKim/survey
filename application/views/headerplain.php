6a0fc9b4142be582e79e34637c181d2d98287c2a
Christian Kim Abac <abackim9087@gmail.com>
2022-12-13 22:06:44 +0800

    kim-update

--- application/views/headerplain.php
+++ application/views/headerplain.php
@@ -6,11 +6,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://unpkg.com/@kfonts/nexon-lv1-gothic-otf/index.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo base_url('resources/css/globalplain.css')?>"/>
     <link rel="stylesheet" href="<?php echo base_url('resources/css/style.css')?>"/>
-    <link rel="stylesheet" href="<?php echo base_url('resources/css/globalcustomize.css')?>"/>
     <script src="<?php echo base_url('resources/jquery/jquery-3.1.1.js')?>"></script>
     <script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script> 
     <title><?php echo $title;?></title>
