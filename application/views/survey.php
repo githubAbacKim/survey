feb6953b825e0908addb2f32b9d12904725c4845
Christian Kim Abac <abackim9087@gmail.com>
2022-12-13 10:30:54 +0800

    kim update

--- application/views/survey.php
+++ application/views/survey.php
@@ -1,6 +1,14 @@
-<div class="main-contatiner">
-    <div class="top-bar"><h1>Survey</h1></div>
+<div class="container-fluid">
+    <div class="row top-bar">
+        <!-- put the codes for the:
+            - title 
+            - pagination tracker
+            - pagination back and next
+        -->
+        <div class="survey_title col-lg-"></div>
+        <div class=""></div>
+    </div>
     <div class="content">
-        <p>This is my survey content</p>
+        <div class=""></div>
     </div>
 </div>
