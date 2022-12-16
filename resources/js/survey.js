a2fb4fdbf58cea680f9bcd7f23bd4b09749f552c
Christian Kim Abac <abackim9087@gmail.com>
2022-12-16 11:15:32 +0800

    kim-update

--- resources/js/survey.js
+++ resources/js/survey.js
@@ -1,77 +1,127 @@
 $(function(){
-    $('#btnSave').click(function(){            
-        $.ajax({
-            type:'ajax',
-            method: 'post',
-            url: url,
-            data: data,
-            async: false,
-            dataType: 'json',
-            success: function(response){
-                var error = response.error;
-                var type = response.type;
-                if (response.success) {
-                    $('#myForm')[0].reset();
-                    $('.alert-success').html(type + 'successful.').fadeIn().delay(2000).fadeOut('slow');
-                    accountTable.ajax.reload(null, false);
-                    if(type === "Update"){$('#myModal').modal('hide');}
-                }else{
-                    $('#myModal').modal('hide');
-                    $('.alert-danger').html(error).fadeIn().delay(2000).fadeOut('slow');
-                }
-            },
-            error: function(){
-                $('.alert-danger').html('Unable to add record.').fadeIn().delay(2000).fadeOut('slow');
-                $('#myModal').modal('hide');
-            }
-        });
-    });
-    
-    var listContainer = $("#slideCont");
-    var paginationcont = $('#template').html();
-
-        var slideIndex = 1;
-        showDivs(slideIndex);
-
-        function plusDivs(n) {
-            showDivs(slideIndex += n);
-        }
-
-        function showDivs(n) {
-            var i;
-            var x = document.getElementsByClassName("mySlides");
-            var y = document.getElementsByClassName("dot");
-            if (n > x.length) { slideIndex = 1 }
-            if (n < 1) { slideIndex = x.length }
-            for (i = 0; i < x.length; i++) {
-                x[i].style.display = "none";
-            }
-            x[slideIndex - 1].style.display = "block";
-            y[slideIndex - 1].style.background = "red";
-        }
-        
+    function getData(){
+        var tmp = null;
         $.ajax({
             url:'../../survey_project/survey/shuffledQuestion',
             async:false,
             dataType:'json',
             success: function(results){
                 $.each(results,function(i,result){
-                    console.log(result.question_num)
-                    var data = {
-                        'qnum': result.question_num,
-                        'question': result.question,
-                        'agree_img': result.agree_image,
-                        'agree_title':'찬성',
-                        'agree_desc': result.agree_desc,
-                        'disagree_img':result.disagree_image,
-                        'disagree_title':'반대',
-                        'disagree_desc':result.disagree_desc,
-                    }
-                    listContainer.append(Mustache.render(paginationcont, data)); 
+                    tmp = results;
                 });
             },
             error: function(){
                 console.log('error')
             }
         });
+        return tmp;
+    }
+    
+    var listContainer = $("#slideCont");
+    var datatemplate = $('#datatemplate').html();
+    
+    function displayData(results){
+        $.each(results,function(i,result){            
+            var data = {
+                'qnum': result.question_num,
+                'question': result.question,
+                'agree_img': result.agree_image,
+                'agree_title':'찬성',
+                'agree_desc': result.agree_desc,
+                'disagree_img':result.disagree_image,
+                'disagree_title':'반대',
+                'disagree_desc':result.disagree_desc,
+            }
+            listContainer.append(Mustache.render(datatemplate, data)); 
+        });
+    }
+
+    function generateSteps(result){
+        var tsteps = result.legth;
+        $.each(result,function(){            
+            $('.progress-indicator').append('<span class="step"></span>');
+        })
+    }
+
+    function showTab(n) {
+        // This function will display the specified tab of the form ...
+        var x = $(".tab");
+        x[n].style.display = "block";
+        // ... and fix the Previous/Next buttons:
+        if (n == 0) {
+            document.getElementById("prevBtn").style.display = "none";
+        } else {
+            document.getElementById("prevBtn").style.display = "inline";
+        }
+        if (n == (x.length - 1)) {
+            document.getElementById("nextBtn").innerHTML = "Submit";
+        } else {
+            document.getElementById("nextBtn").innerHTML = "Next";
+        }
+        // ... and run a function that displays the correct step indicator:
+        fixStepIndicator(n)
+    }
+
+    function nextPrev(n) {
+        // This function will figure out which tab to display
+        var x = document.getElementsByClassName("tab");
+        // Exit the function if any field in the current tab is invalid:
+        if (n == 1 && !validateForm()) return false;
+        // Hide the current tab:
+        x[currentTab].style.display = "none";
+        // Increase or decrease the current tab by 1:
+        currentTab = currentTab + n;
+        // if you have reached the end of the form... :
+        if (currentTab >= x.length) {
+            //...the form gets submitted:
+            document.getElementById("regForm").submit();
+            return false;
+        }
+        // Otherwise, display the correct tab:
+        showTab(currentTab);
+    }
+
+    function validateForm() {
+        // This function deals with validation of the form fields
+        var x, y, i, valid = true;
+        x = document.getElementsByClassName("tab");
+        y = x[currentTab].getElementsByTagName("input");
+        // A loop that checks every input field in the current tab:
+        for (i = 0; i < y.length; i++) {
+            // If a field is empty...
+            if (y[i].value == "") {
+            // add an "invalid" class to the field:
+            y[i].className += " invalid";
+            // and set the current valid status to false:
+            valid = false;
+            }
+        }
+        // If the valid status is true, mark the step as finished and valid:
+        if (valid) {
+            document.getElementsByClassName("step")[currentTab].className += " finish";
+        }
+        return valid; // return the valid status
+    }
+
+    function fixStepIndicator(n) {
+        // This function removes the "active" class of all steps...
+        var i, x = document.getElementsByClassName("step");
+        for (i = 0; i < x.length; i++) {
+            x[i].className = x[i].className.replace(" active", "");
+        }
+        //... and adds the "active" class to the current step:
+        x[n].className += " active";
+    }
+
+    console.log(getData());
+    displayData(getData());
+    generateSteps(getData());
+    var currentTab = 0; // Current tab is set to be the first tab (0)
+    showTab(currentTab); // Display the current tab
+    $("#prevBtn").click(function(){
+        nextPrev(-1);
+    });
+    $("#nextBtn").click(function(){
+        nextPrev(1)
+    });
 })
