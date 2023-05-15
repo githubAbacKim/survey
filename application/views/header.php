969cdf23fbb408ce14882124bb9f5d030f3a3aa6
Christian Kim Abac <abackim9087@gmail.com>
2023-05-15 21:36:38 +0800

    new update

--- application/views/header.php
+++ application/views/header.php
@@ -8,6 +8,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="naver-site-verification" content="<?php echo base_url("resources/verification/naveref7fb0c94523639716090d177a8e6b35.html")?>" />
+    <meta name="google-site-verification" content="<?php echo base_url("resources/verification/googled1783da29af15b9e.html")?>" />
     <link rel="stylesheet" href="https://unpkg.com/@kfonts/nexon-lv1-gothic-otf/index.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo base_url('resources/css/style.css') ?>" />
